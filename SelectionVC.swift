4840517b9596b9a48c433f699c51633f4f101946
Aayushsood7 <78071226+Aayushsood7@users.noreply.github.com>
2021-12-30 13:12:06 -0800

    Update SelectionVC.swift

--- SelectionVC.swift
+++ SelectionVC.swift
@@ -1,1 +1,39 @@
+import UIKit
+
+class SelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = false
+    }
+    
+
+    @IBAction func LoginBtnAction(_ sender: Any) {
+        
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "LoginVC") as! LoginVC
+        self.navigationController?.pushViewController(vc, animated: true)
+
+    }
+    @IBAction func CreateBtnAction(_ sender: Any) {let vc = self.storyboard?.instantiateViewController(withIdentifier: "CreateProfileVC") as! CreateProfileVC
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
 
