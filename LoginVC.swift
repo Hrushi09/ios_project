010324503acbf9a82f509d971b39e6e7f3e4fb15
Paramjit99 <82598723+Paramjit99@users.noreply.github.com>
2021-12-31 19:32:54 -0500

    Update LoginVC.swift

--- LoginVC.swift
+++ LoginVC.swift
@@ -1,1 +1,30 @@
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signinBtnAction(_ sender: Any) {
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "DashboardVC") as! DashboardVC
+            self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func forgotBtnAction(_ sender: Any) {let vc = self.storyboard?.instantiateViewController(withIdentifier: "ForgotPasswordVC") as! ForgotPasswordVC
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
 
