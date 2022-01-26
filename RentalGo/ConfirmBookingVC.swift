b0442c2997758d983cc5c051c714c0a18ffeabc6
Aayushsood7 <78071226+Aayushsood7@users.noreply.github.com>
2022-01-25 20:02:53 -0800

    Update ConfirmBookingVC.swift

--- RentalGo/ConfirmBookingVC.swift
+++ RentalGo/ConfirmBookingVC.swift
@@ -1,1 +1,46 @@
+import UIKit
+import Cosmos
+
+class ConfirmBookingVC: UIViewController {
+
+    //MARK:- Outlet
+    
+    @IBOutlet weak var cosmosView: CosmosView!
+    @IBOutlet weak var lblMsg : UILabel!
+    @IBOutlet weak var btnContinue : UIButton!
+    
+    //MARK:- Class Variable
+    
+    //MARK:- Custom Method
+    
+    func setUpView(){
+        self.applyStyle()
+        self.cosmosView.didFinishTouchingCosmos =  { rating in
+            self.btnContinue.isHidden = false
+            self.lblMsg.isHidden = false
+        }
+    }
+    
+    func applyStyle(){
+        
+    }
+    
+    //MARK:- Action Method
+    @IBAction func btnContinueTapped(_ sender: Any) {
+        let viewControllers: [UIViewController] = self.navigationController!.viewControllers
+        for aViewController in viewControllers {
+            if aViewController is HomeVC {
+                self.navigationController!.popToViewController(aViewController, animated: true)
+            }
+        }
+    }
+    
+    //MARK:- Delegates
+    
+    //MARK:- UILifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setUpView()
+    }
+}
 
