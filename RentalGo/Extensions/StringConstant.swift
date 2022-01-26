f478dc85548c6e0614e6fd0df346379e933d645b
Hrushi09 <74370312+Hrushi09@users.noreply.github.com>
2022-01-25 23:00:14 -0500

    Update StringConstant.swift

--- RentalGo/Extensions/StringConstant.swift
+++ RentalGo/Extensions/StringConstant.swift
@@ -1,1 +1,24 @@
 
+
+
+import Foundation
+
+
+// Tables name
+let gUser               = "users"
+let gfullName           = "fullname"
+let gHotel              = "Hotel_list"
+let gOrder              = "order"
+let gEmail              = "email"
+let gPassword           = "password"
+let gConfirmPassword    = "ConfirmPassword"
+let gServerTime         = "timeStamp"
+let gImages             = "image"
+let gPrice              = "price"
+let gId                 = "id"
+let gName               = "name"
+let gIsAvailable        = "isAvailable"
+let gIsBooked           = "isBooked"
+let gstatus             = "status"
+let gHotelNumber        = "hotelNumber"
+let gIsCancelled        = "isCancelled"
