0205a08c16d1ec0f42726baf304dc7cb4e5507ec
Aayushsood7 <78071226+Aayushsood7@users.noreply.github.com>
2022-01-25 19:59:19 -0800

    Update CancelledVC.swift

--- RentalGo/CancelledVC.swift
+++ RentalGo/CancelledVC.swift
@@ -1,1 +1,19 @@
+import UIKit
+
+class CancelledVC: UIViewController {
+
+    
+    @IBOutlet weak var btnContinue : UIButton!
+    
+    @IBAction func btnContinueClick(_ sender: Any) {
+        let vc = storyboard?.instantiateViewController(withClass: HomeVC.self)
+        self.navigationController?.pushViewController(vc!, animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.btnContinue.layer.cornerRadius = 15
+        // Do any additional setup after loading the view.
+    }
+}
 
